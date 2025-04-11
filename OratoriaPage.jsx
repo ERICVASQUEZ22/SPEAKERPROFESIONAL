1257a0d17a461a219d023d55a8c07fb7e2fe7626
ERICVASQUEZ22 <ericxhavahe22@gmail.com>
2025-04-11 11:51:40 -0500

    Add files via upload

---
+++ OratoriaPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function OratoriaPage() {
+  return (
+    <div>
+      <h1>Bienvenido a tu nueva página de oratoria</h1>
+      <p>Ofrecemos servicios, cursos y charlas para potenciar tu voz.</p>
+    </div>
+  )
+}
---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import OratoriaPage from './OratoriaPage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OratoriaPage />
+  </React.StrictMode>
+)
